41614a5f1438daf216ac6dc17c559198a77dec38
LW <568270833@qq.com>
2025-11-04 16:13:18 +0800

    Initial Commit

---
+++ WhatToEat/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  WhatToEat
+//
+//  Created by 王敬霖 on 2025/11/4.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ WhatToEat/WhatToEatApp.swift
@@ -0,0 +1,17 @@
+//
+//  WhatToEatApp.swift
+//  WhatToEat
+//
+//  Created by 王敬霖 on 2025/11/4.
+//
+
+import SwiftUI
+
+@main
+struct WhatToEatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
